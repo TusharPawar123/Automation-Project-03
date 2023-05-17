df366e80a5f28f81f33466b964658c3d5e697fa0
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-17 10:54:35 +0530

    my third commit

---
+++ Multiplication/mul.java
@@ -0,0 +1,14 @@
+package Multiplication;
+
+public class mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 4;
+
+int mul = (a*b);
+System.out.println(mul);
+	}
+
+}
